f6018dd9434a4c15244ac6fb45741724d310f5aa
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-08-21 16:12:22 +0200

    GoSemSim clastering + gene age results

--- code/07_GO_SemSim.R
+++ code/07_GO_SemSim.R
@@ -5,13 +5,14 @@ library(clusterProfiler)
 library(reshape2)
 library(ggplot2)
 
-human_genes = readLines("../data/04_genes_lists/human/human.txt")
+human_genes.df = read.csv(file = '../data/08_GO_gene_subset/ages_lists/human_genes_id.csv',
+                       col.names = c('SYMBOL', 'ENTREZID'),
+                       colClasses = "character")
+eg <- human_genes.df['ENTREZID'][, 1]
 
+# MF
 hsGO_MF <- godata('org.Hs.eg.db', ont="MF", computeIC = FALSE)
-human_genes.df <- bitr(human_genes, "SYMBOL", "ENTREZID", "org.Hs.eg.db")
-human_genes <- human_genes.df[,1]
-eg <- human_genes.df[,2]
-names(human_genes) <- eg
+
 
 sim_MF <- mgeneSim(eg, semData = hsGO_MF, measure = "Wang", drop = "IEA", combine = "BMA")
 rownames(sim_MF) <- human_genes[rownames(sim_MF)]
@@ -50,9 +51,22 @@ colnames(sim_BP) <- human_genes[colnames(sim_BP)]
 
 write.table(sim_BP, file="../data/07_GO/human_sim_BP.txt")
 
+## read and plot
 sim_BP <- as.matrix(read.table("../data/07_GO/human_sim_BP.txt", header=TRUE, row.names=1))
 melted_sim_BP <- melt(sim_BP)
 
+# Hierarchical Cluster Analysis
+dist_BP <- dist(sim_BP)
+# https://uc-r.github.io/hc_clustering#algorithms
+hc.complete <- hclust(dist_BP, method = 'complete') 
+hc.ward.D2 <- hclust(dist_BP, method = "ward.D2")
+## save
+dend.complete <- as.phylo(hc.complete)
+dend.ward.D2 <- as.phylo(hc.ward.D2)
+write.tree(dend.complete, file = "../data/07_GO/dendrograms/humanBP_dend_complete.tree")
+write.tree(dend.ward.D2, file = "../data/07_GO/dendrograms/humanBP_dend_ward.tree")
+
+# plots
 png("../data/07_GO_plots/human_BP_01.png", width = 5200, height = 5000, units = "px")
 heatmap(sim_BP, Rowv = NA)
 dev.off()
@@ -106,13 +120,14 @@ rm(sim_CC)
 #############################################################################
 
 # MOUSE MF
-mouse_genes = readLines("../data/04_genes_lists/mouse/mouse.txt")
+mouse_genes.df = read.csv(file = '../data/08_GO_gene_subset/ages_lists/mouse_genes_id.csv',
+                          col.names = c('SYMBOL', 'ENTREZID'),
+                          colClasses = "character")
+eg <- mouse_genes.df['ENTREZID'][, 1]
 
+# MF
 hsGO_MF <- godata('org.Mm.eg.db', ont="MF", computeIC = FALSE)
-mouse_genes.df <- bitr(mouse_genes, "SYMBOL", "ENTREZID", "org.Mm.eg.db")
-mouse_genes <- mouse_genes.df[,1]
-eg <- mouse_genes.df[,2]
-names(mouse_genes) <- eg
+
 
 sim_MF <- mgeneSim(eg, semData = hsGO_MF, measure = "Wang", drop = "IEA", combine = "BMA")
 rownames(sim_MF) <- mouse_genes[rownames(sim_MF)]
@@ -151,9 +166,21 @@ colnames(sim_BP) <- mouse_genes[colnames(sim_BP)]
 
 write.table(sim_BP, file="../data/07_GO/mouse_sim_BP.txt")
 
-sim_BP <- as.matrix(read.table("../data/07_GO/mouse_sim_BP.txt", header=TRUE, row.names=1))
-melted_sim_BP <- melt(sim_BP)
+sim_BP_mouse <- as.matrix(read.table("../data/07_GO/mouse_sim_BP.txt", header=TRUE, row.names=1))
+melted_sim_BP <- melt(sim_BP_mouse)
 
+# Hierarchical Cluster Analysis
+dist_BP <- dist(sim_BP)
+# https://uc-r.github.io/hc_clustering#algorithms
+hc.complete <- hclust(dist_BP, method = 'complete') 
+hc.ward.D2 <- hclust(dist_BP, method = "ward.D2")
+## save
+dend.complete <- as.phylo(hc.complete)
+dend.ward.D2 <- as.phylo(hc.ward.D2)
+write.tree(dend.complete, file = "../data/07_GO/dendrograms/mouseBP_dend_complete.tree")
+write.tree(dend.ward.D2, file = "../data/07_GO/dendrograms/mouseBP_dend_ward.tree")
+
+# plots
 png("../data/07_GO_plots/mouse_BP_01.png", width = 5200, height = 5000, units = "px")
 heatmap(sim_BP, Rowv = NA)
 dev.off()
